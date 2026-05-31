d0929c4a5676ec2259d1e773183ae09400bc4ec3
SHUBHAM TRIPATHI <shubhamtripathi15199@gmail.com>
2026-05-31 15:09:26 +0530

    Initial Commit

---
+++ src/main/java/com/outreachengine/OutreachEngineApplication.java
@@ -0,0 +1,13 @@
+package com.outreachengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutreachEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutreachEngineApplication.class, args);
+	}
+
+}
